feead8f97abda221b81dbd7e6d9442c339ffde3e
Yuki Kimura <kikikimu.lab@gmail.com>
2019-01-19 13:18:59 +0900

    remove space

--- mylib/photo2exif.py
+++ mylib/photo2exif.py
@@ -7,7 +7,7 @@ def getExif(file):
     exif = image._getexif()
     if exit is None:
         return
-    
+
     exif_data = {}
     for tag_id, value in exif.items():
         tag = TAGS.get(tag_id, tag_id)
@@ -17,11 +17,11 @@ def getExif(file):
             for t in value:
                 gps_tag = GPSTAGS.get(t,t)
                 gps_data[gps_tag] = value[t]
-            
+
             exif_data[tag] = gps_data
         else:
             exif_data[tag] = value
-    
+
     return exif_data
 
 if __name__ == '__main__':
